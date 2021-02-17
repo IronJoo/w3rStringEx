082e018886c6619d0dec091d0ecad7eab0fe2052
IronJoo <joanaferro92@gmail.comgit commit -m First>
2021-02-17 21:58:32 +0000

    First Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace w3rStringEx //{}
+{
+    class Exercise1
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Input text: ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"The string you entered is: {input}");
+            Console.ReadLine();
+        }
+    }
+}
